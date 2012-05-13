2a8d3b50f2ceda423c52f8a198b54706293f0884
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2012-05-13 21:45:26 +0700

    readme changed

--- Brainfuck.php
+++ Brainfuck.php
@@ -2,6 +2,7 @@
 /*
  * Copyright 2012, Brainfuck Wiki Extension
  *
+ * Version: 0.2
  * Author: Vladimir Kostyukov <vladimir.kostukov@gmail.com>
  * License: http://www.apache.org/licenses/LICENSE-2.0.html
  *
